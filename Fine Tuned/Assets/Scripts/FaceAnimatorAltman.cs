4cfd59f42e516cdddec8379f9a26d8c5d996f3f8
SirFatredVIII <dhall25@huskers.unl.edu>
2025-03-02 07:13:34 -0600

    ending credits trigger

---
+++ Fine Tuned/Assets/Scripts/FaceAnimatorAltman.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FaceAnimatorAltman : MonoBehaviour
+{
+    private Renderer faceRenderer;
+    public Material sadFace;
+    public Material happyFace;
+    public Material idleFace;
+
+    public GameObject face;
+
+    // Reference to the Dialogue script
+    private Dialogue dialogue;
+
+    public GameObject canvas;
+
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the Dialogue component on the same GameObject
+        dialogue = GetComponent<Dialogue>();
+
+        // Find the fcae GameObject and its renderer
+        // heh heh. lmao, even!
+
+        faceRenderer = face.GetComponent<MeshRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFaceBasedOnDialogue();
+    }
+
+    void UpdateFace(Material newFace)
+    {
+        var new_materials = faceRenderer.materials;
+        new_materials[0] = newFace;
+        faceRenderer.materials = new_materials;
+        
+    }
+
+    void UpdateFaceBasedOnDialogue()
+    {
+        // Assuming dialogue.currentOptionIndex gives the current dialogue option index
+        
+        int currentOptionIndex = dialogue.GetCurrentOptionIndex();
+        // Update the face based on the current dialogue option
+        switch (currentOptionIndex)
+        {
+            case 7: // empty
+                canvas.SetActive(true);
+                break;
+            default:
+                UpdateFace(idleFace); // Default to idle face
+                break;
+        }
+    }
+}
